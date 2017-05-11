6ca76168e18c7b6ecc5cd0a14024f3b3e8b00ca3
Dag Ågren <dag.agren@wolt.com>
2017-05-11 13:19:08 +0300

    Initial Commit

---
+++ Blurhash/Blurhash.h
@@ -0,0 +1,19 @@
+//
+//  Blurhash.h
+//  Blurhash
+//
+//  Created by Dag Ågren on 11/05/2017.
+//  Copyright © 2017 Dag Ågren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blurhash.
+FOUNDATION_EXPORT double BlurhashVersionNumber;
+
+//! Project version string for Blurhash.
+FOUNDATION_EXPORT const unsigned char BlurhashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blurhash/PublicHeader.h>
+
+
