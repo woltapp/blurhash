7666ef518fc7c667561da2d8a38983372ec38759
Dag Ågren <dag.agren@wolt.com>
2018-05-30 14:30:56 +0300

    Slighty more efficient decoding

--- Swift/BlurHashKit/StringCoding.swift
+++ Swift/BlurHashKit/StringCoding.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-private let digitCharacters = [
+private let encodeCharacters = [
 	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
 	"k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
@@ -10,12 +10,20 @@ private let digitCharacters = [
 	"Y", "Z", ":", ";"
 ]
 
+private let decodeCharacters: [String: Int] = {
+    var dict: [String: Int] = [:]
+    for (index, character) in encodeCharacters.enumerated() {
+        dict[character] = index
+    }
+    return dict
+}()
+
 extension BinaryInteger {
 	func encode64(length: Int) -> String {
 		var result = ""
 		for i in 1 ... length {
 			let digit = (self >> (6 * (length - i))) & 63
-			result += digitCharacters[Int(digit)]
+			result += encodeCharacters[Int(digit)]
 		}
 		return result
 	}
@@ -25,7 +33,7 @@ extension String {
 	func decode64() -> Int {
 		var value: Int = 0
 		for character in self {
-			if let digit = digitCharacters.index(of: String(character)) {
+			if let digit = decodeCharacters[String(character)] {
 				value = (value << 6) + digit
 			}
 		}
