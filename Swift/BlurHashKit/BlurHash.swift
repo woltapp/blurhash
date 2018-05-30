f794386c524f79d1cc0cc9e09eb409b726b00d0d
Dag Ågren <dag.agren@wolt.com>
2018-05-30 12:31:05 +0300

    Updates for newer Xcode and Swift

--- Swift/BlurHashKit/BlurHash.swift
+++ Swift/BlurHashKit/BlurHash.swift
@@ -21,17 +21,17 @@ public struct BlurHash {
 
 public func +(lhs: BlurHash, rhs: BlurHash) throws -> BlurHash {
 	return BlurHash(components: paddedZip(lhs.components, rhs.components, [], []).map {
-		paddedZip($0.0, $0.1, (0, 0, 0), (0, 0, 0)).map { ($0.0.0 + $0.1.0, $0.0.1 + $0.1.1, $0.0.2 + $0.1.2) }
+		paddedZip($0.0, $0.1, (0, 0, 0) as (Float, Float, Float), (0, 0, 0) as (Float, Float, Float)).map { ($0.0.0 + $0.1.0, $0.0.1 + $0.1.1, $0.0.2 + $0.1.2) }
 	})
 }
 
 public func -(lhs: BlurHash, rhs: BlurHash) throws -> BlurHash {
 	return BlurHash(components: paddedZip(lhs.components, rhs.components, [], []).map {
-		paddedZip($0.0, $0.1, (0, 0, 0), (0, 0, 0)).map { ($0.0.0 - $0.1.0, $0.0.1 - $0.1.1, $0.0.2 - $0.1.2) }
+		paddedZip($0.0, $0.1, (0, 0, 0) as (Float, Float, Float), (0, 0, 0) as (Float, Float, Float)).map { ($0.0.0 - $0.1.0, $0.0.1 - $0.1.1, $0.0.2 - $0.1.2) }
 	})
 }
 
-private func paddedZip<Collection1, Collection2>(_ collection1: Collection1, _ collection2: Collection2, _ padding1: Collection1.Element, _ padding2: Collection2.Element) -> Zip2Sequence<[Collection1.Element], [Collection2.Element]> where Collection1: Collection, Collection2: Collection, Collection1.IndexDistance == Int, Collection2.IndexDistance == Int {
+private func paddedZip<Collection1, Collection2>(_ collection1: Collection1, _ collection2: Collection2, _ padding1: Collection1.Element, _ padding2: Collection2.Element) -> Zip2Sequence<[Collection1.Element], [Collection2.Element]> where Collection1: Collection, Collection2: Collection {
 	if collection1.count < collection2.count {
 		let padded = collection1 + Array(repeating: padding1, count: collection2.count - collection1.count)
 		return zip(padded, Array(collection2))
