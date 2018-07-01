4e482de275949e73b936e21f868553abeae65566
Sergey Ushakov <sergey.ushakov@wolt.com>
2018-07-01 13:25:33 +0300

    caculated blurhashes, added canvases and blurhash rendering

--- Website/src/index.js
+++ Website/src/index.js
@@ -1,1 +1,31 @@
 import './index.scss';
+import { imageHashes } from './constants';
+import decode from '../../TypeScript/dist/decode';
+
+document.addEventListener('readystatechange', drawBlurHash);
+
+function render(canvas, blurhash) {
+  if (blurhash) {
+    const pixels = decode(blurhash, 32, 32);
+    if (pixels) {
+      const ctx = canvas.getContext('2d');
+
+      const imageData = new ImageData(pixels, 32, 32);
+      ctx.putImageData(imageData, 0, 0);
+    }
+  }
+}
+
+function drawBlurHash() {
+  if (!document.readyState === 'complete') {
+    return;
+  }
+
+  const canvases = document.getElementsByClassName('image-canvas-bg');
+
+  if (canvases && canvases.length) {
+    for (let i = 0; i < canvases.length; i++) {
+      render(canvases[i], imageHashes[i]);
+    }
+  }
+}
