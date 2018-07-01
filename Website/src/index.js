9cdc36e84bd7a763f7bac4cf2f1f564c42f96b0e
Sergey Ushakov <sergey.ushakov@wolt.com>
2018-07-01 16:38:30 +0300

    added animations for first section

--- Website/src/index.js
+++ Website/src/index.js
@@ -1,8 +1,12 @@
 import './index.scss';
 import { imageHashes } from './constants';
 import decode from '../../TypeScript/dist/decode';
+import Velocity from 'velocity-animate';
 
-document.addEventListener('readystatechange', drawBlurHash);
+// document.addEventListener('readystatechange', drawBlurHash);
+const images = document.getElementsByClassName('image-bg');
+const imageContainer = document.getElementsByClassName('imagesContainer');
+const content = document.getElementsByClassName('content');
 
 function render(canvas, blurhash) {
   if (blurhash) {
@@ -15,12 +19,12 @@ function render(canvas, blurhash) {
     }
   }
 }
-
+drawBlurHash();
 function drawBlurHash() {
   if (!document.readyState === 'complete') {
     return;
   }
-
+  init();
   const canvases = document.getElementsByClassName('image-canvas-bg');
 
   if (canvases && canvases.length) {
@@ -28,4 +32,45 @@ function drawBlurHash() {
       render(canvases[i], imageHashes[i]);
     }
   }
+  startAnimation();
+}
+
+function init() {
+  Velocity({
+    elements: imageContainer,
+    properties: { translateY: '25%' },
+    options: {
+      duration: 0,
+    },
+  });
+  Velocity({
+    elements: content,
+    properties: { opacity: 0 },
+    options: {
+      duration: 0,
+    },
+  });
+}
+
+function startAnimation() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add('animateImages');
+  }
+  Velocity({
+    elements: imageContainer,
+    properties: { translateY: '0%' },
+    options: {
+      duration: 750,
+      delay: 500,
+      easing: 'easeInOutCubic',
+    },
+  });
+  Velocity({
+    elements: content,
+    properties: { opacity: 1 },
+    options: {
+      duration: 250,
+      delay: 1000,
+    },
+  });
 }
