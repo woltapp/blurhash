f4db952f568fbb276a97830e22dc3f03ac6aaf84
Sergey Ushakov <sergey.ushakov@wolt.com>
2018-07-01 13:25:33 +0300

    caculated blurhashes, added canvases and blurhash rendering

---
+++ Website/src/constants.js
@@ -0,0 +1,9 @@
+const imageHashes = [
+  'LEHV6njZ2ykUpyoKadR*.8kCMdnj',
+  'LHF5]+c[^6#M@-5b,1J5@[or[kA{',
+  'L6Pj0^xZ.A.S_Nt7t7R+*0o}DgQ-',
+  'LKO2?U%2Tw=_]~VeVZRi};RPxuwH',
+  'LPPGdFog?wt7?HofM|R+OGRjr;xu',
+];
+
+export { imageHashes };
