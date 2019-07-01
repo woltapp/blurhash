bf402e4df803632d5cd73d3afbef0225e0173508
dean <hangduykhiem@gmail.com>
2019-07-01 10:38:04 +0300

    Kt: Basic demo application

--- Kotlin/demo/src/androidTest/java/com/wolt/blurhashapp/ExampleInstrumentedTest.kt
+++ Kotlin/demo/src/androidTest/java/com/wolt/blurhashapp/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.wolt.blurhashapp
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
--- Kotlin/demo/src/main/java/com/wolt/blurhashapp/MainActivity.kt
+++ Kotlin/demo/src/main/java/com/wolt/blurhashapp/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.wolt.blurhashapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.wolt.blurhashkt.BlurHashDecoder.decode
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etBlurHash: EditText
+    private lateinit var tvDecode: TextView
+    private lateinit var ivBlurHash: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etBlurHash = findViewById(R.id.etBlurHash)
+        tvDecode = findViewById(R.id.tvDecode)
+        ivBlurHash = findViewById(R.id.ivBlurHash)
+
+        tvDecode.setOnClickListener { decodeBlurHash(etBlurHash.text.toString()) }
     }
+
+    private fun decodeBlurHash(blurHashString: String) {
+        Thread(Runnable {
+            val bitmap = decode(blurHashString, 20, 20, 1f)
+            ivBlurHash.post { ivBlurHash.setImageBitmap(bitmap) }
+        }).start()
+    }
+
 }
