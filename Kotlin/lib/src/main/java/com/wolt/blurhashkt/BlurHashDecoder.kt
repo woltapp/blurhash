2d350e8f3ded37bdde6688b7e6af1fbe57e908af
Konrad Pozniak <k.pozniak@gmx.at>
2019-07-02 12:13:39 +0200

    add MAX_SIZE again

--- Kotlin/lib/src/main/java/com/wolt/blurhashkt/BlurHashDecoder.kt
+++ Kotlin/lib/src/main/java/com/wolt/blurhashkt/BlurHashDecoder.kt
@@ -9,6 +9,8 @@ import kotlin.math.*
  */
 object BlurHashDecoder {
 
+    val MAX_SIZE = 20
+
     private val digitCharacters = arrayOf(
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
