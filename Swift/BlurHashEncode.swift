0a0c2fd2e315a0a464e19e94e0a08ed1fa99abbf
Dag Ågren <paracelsus@gmail.com>
2020-01-15 23:04:19 +0200

    Convert images to sRGB before encoding in the Swift versions.

--- Swift/BlurHashEncode.swift
+++ Swift/BlurHashEncode.swift
@@ -2,15 +2,32 @@ import UIKit
 
 extension UIImage {
     public func blurHash(numberOfComponents components: (Int, Int)) -> String? {
-        guard components.0 >= 1, components.0 <= 9,
-        components.1 >= 1, components.1 <= 9,
-        cgImage?.colorSpace?.numberOfComponents == 3,
-        cgImage?.bitsPerPixel == 24 || cgImage?.bitsPerPixel == 32 else { return nil }
+		let pixelWidth = Int(round(size.width * scale))
+		let pixelHeight = Int(round(size.height * scale))
 
-        guard let cgImage = cgImage,
-        let dataProvider = cgImage.dataProvider,
-        let data = dataProvider.data,
-        let pixels = CFDataGetBytePtr(data) else { return nil }
+		let context = CGContext(
+			data: nil,
+			width: pixelWidth,
+			height: pixelHeight,
+			bitsPerComponent: 8,
+			bytesPerRow: pixelWidth * 4,
+			space: CGColorSpace(name: CGColorSpace.sRGB)!,
+			bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+		)!
+		context.scaleBy(x: scale, y: -scale)
+		context.translateBy(x: 0, y: -size.height)
+
+		UIGraphicsPushContext(context)
+		draw(at: .zero)
+		UIGraphicsPopContext()
+
+		guard let cgImage = context.makeImage(),
+		let dataProvider = cgImage.dataProvider,
+		let data = dataProvider.data,
+		let pixels = CFDataGetBytePtr(data) else {
+			assertionFailure("Unexpected error!")
+			return nil
+		}
 
         let width = cgImage.width
         let height = cgImage.height
--- Swift/BlurHashKit/FromUIImage.swift
+++ Swift/BlurHashKit/FromUIImage.swift
@@ -7,13 +7,30 @@ public extension BlurHash {
 			fatalError("Number of components bust be between 1 and 9 inclusive on each axis")
 		}
 
-		guard let cgImage = image.cgImage,
+		let pixelWidth = Int(round(image.size.width * image.scale))
+		let pixelHeight = Int(round(image.size.height * image.scale))
+
+		let context = CGContext(
+			data: nil,
+			width: pixelWidth,
+			height: pixelHeight,
+			bitsPerComponent: 8,
+			bytesPerRow: pixelWidth * 4,
+			space: CGColorSpace(name: CGColorSpace.sRGB)!,
+			bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+		)!
+		context.scaleBy(x: image.scale, y: -image.scale)
+		context.translateBy(x: 0, y: -image.size.height)
+
+		UIGraphicsPushContext(context)
+		image.draw(at: .zero)
+		UIGraphicsPopContext()
+
+		guard let cgImage = context.makeImage(),
 		let dataProvider = cgImage.dataProvider,
 		let data = dataProvider.data,
-		let pixels = CFDataGetBytePtr(data),
-		cgImage.colorSpace?.numberOfComponents == 3,
-		cgImage.bitsPerPixel == 24 || cgImage.bitsPerPixel == 32 else {
-			assertionFailure("Invalid image format")
+		let pixels = CFDataGetBytePtr(data) else {
+			assertionFailure("Unexpected error!")
 			return nil
 		}
 
@@ -24,14 +41,14 @@ public extension BlurHash {
 		self.components = (0 ..< components.1).map { j -> [(Float, Float, Float)] in
 			return (0 ..< components.0).map { i -> (Float, Float, Float) in
 				let normalisation: Float = (i == 0 && j == 0) ? 1 : 2
-				return BlurHash.multiplyBasisFunction(pixels: pixels, width: width, height: height, bytesPerRow: bytesPerRow, bytesPerPixel: cgImage.bitsPerPixel / 8, pixelOffset: 0) { x, y in
+				return BlurHash.multiplyBasisFunction(pixels: pixels, width: width, height: height, bytesPerRow: bytesPerRow, bytesPerPixel: cgImage.bitsPerPixel / 8) { x, y in
 					normalisation * cos(Float.pi * Float(i) * x / Float(width)) as Float * cos(Float.pi * Float(j) * y / Float(height)) as Float
 				}
 			}
 		}
 	}
 
-	static private func multiplyBasisFunction(pixels: UnsafePointer<UInt8>, width: Int, height: Int, bytesPerRow: Int, bytesPerPixel: Int, pixelOffset: Int, basisFunction: (Float, Float) -> Float) -> (Float, Float, Float) {
+	static private func multiplyBasisFunction(pixels: UnsafePointer<UInt8>, width: Int, height: Int, bytesPerRow: Int, bytesPerPixel: Int, basisFunction: (Float, Float) -> Float) -> (Float, Float, Float) {
 		var c: (Float, Float, Float) = (0, 0, 0)
 
 		let buffer = UnsafeBufferPointer(start: pixels, count: height * bytesPerRow)
@@ -39,9 +56,9 @@ public extension BlurHash {
 		for x in 0 ..< width {
 			for y in 0 ..< height {
 				c += basisFunction(Float(x), Float(y)) * (
-					sRGBToLinear(buffer[bytesPerPixel * x + pixelOffset + 0 + y * bytesPerRow]),
-					sRGBToLinear(buffer[bytesPerPixel * x + pixelOffset + 1 + y * bytesPerRow]),
-					sRGBToLinear(buffer[bytesPerPixel * x + pixelOffset + 2 + y * bytesPerRow])
+					sRGBToLinear(buffer[bytesPerPixel * x + 0 + y * bytesPerRow]),
+					sRGBToLinear(buffer[bytesPerPixel * x + 1 + y * bytesPerRow]),
+					sRGBToLinear(buffer[bytesPerPixel * x + 2 + y * bytesPerRow])
 				)
 			}
 		}
