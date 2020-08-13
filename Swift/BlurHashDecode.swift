cf74ca7956790f1402c9b7ada8c3513ef944393e
Paul Griffin <paul.griffin@svt.se>
2020-08-13 10:29:06 +0200

    Improved performance of swift decode by 2-4x by caching results from cos operations

--- Swift/BlurHashDecode.swift
+++ Swift/BlurHashDecode.swift
@@ -4,18 +4,18 @@ extension UIImage {
     public convenience init?(blurHash: String, size: CGSize, punch: Float = 1) {
         guard blurHash.count >= 6 else { return nil }
 
-		let sizeFlag = String(blurHash[0]).decode83()
-		let numY = (sizeFlag / 9) + 1
-		let numX = (sizeFlag % 9) + 1
+        let sizeFlag = String(blurHash[0]).decode83()
+        let numY = (sizeFlag / 9) + 1
+        let numX = (sizeFlag % 9) + 1
 
-		let quantisedMaximumValue = String(blurHash[1]).decode83()
+        let quantisedMaximumValue = String(blurHash[1]).decode83()
         let maximumValue = Float(quantisedMaximumValue + 1) / 166
 
         guard blurHash.count == 4 + 2 * numX * numY else { return nil }
 
         let colours: [(Float, Float, Float)] = (0 ..< numX * numY).map { i in
             if i == 0 {
-				let value = String(blurHash[2 ..< 6]).decode83()
+                let value = String(blurHash[2 ..< 6]).decode83()
                 return decodeDC(value)
             } else {
                 let value = String(blurHash[4 + i * 2 ..< 4 + i * 2 + 2]).decode83()
@@ -30,25 +30,40 @@ extension UIImage {
         CFDataSetLength(data, bytesPerRow * height)
         guard let pixels = CFDataGetMutableBytePtr(data) else { return nil }
 
+        let cosxi = (0 ..< width).map { x in
+            (0 ..< numX).map { i in
+                cos(Float.pi * Float(x) * Float(i) / Float(width))
+            }
+        }
+
+        let cosyj = (0 ..< height).map { y in
+            (0 ..< numY).map { j in
+                cos(Float.pi * Float(y) * Float(j) / Float(height))
+            }
+        }
+
         for y in 0 ..< height {
+            let cosj = cosyj[y]
             for x in 0 ..< width {
-                var r: Float = 0
-                var g: Float = 0
-                var b: Float = 0
+                let cosi = cosxi[x]
+
+                var red: Float = 0
+                var green: Float = 0
+                var blue: Float = 0
 
                 for j in 0 ..< numY {
                     for i in 0 ..< numX {
-                        let basis = cos(Float.pi * Float(x) * Float(i) / Float(width)) * cos(Float.pi * Float(y) * Float(j) / Float(height))
+                        let basis = cosi[i] * cosj[j]
                         let colour = colours[i + j * numX]
-                        r += colour.0 * basis
-                        g += colour.1 * basis
-                        b += colour.2 * basis
+                        red += colour.0 * basis
+                        green += colour.1 * basis
+                        blue += colour.2 * basis
                     }
                 }
 
-                let intR = UInt8(linearTosRGB(r))
-                let intG = UInt8(linearTosRGB(g))
-                let intB = UInt8(linearTosRGB(b))
+                let intR = UInt8(linearTosRGB(red))
+                let intG = UInt8(linearTosRGB(green))
+                let intB = UInt8(linearTosRGB(blue))
 
                 pixels[3 * x + 0 + y * bytesPerRow] = intR
                 pixels[3 * x + 1 + y * bytesPerRow] = intG
@@ -59,8 +74,17 @@ extension UIImage {
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
 
         guard let provider = CGDataProvider(data: data) else { return nil }
-        guard let cgImage = CGImage(width: width, height: height, bitsPerComponent: 8, bitsPerPixel: 24, bytesPerRow: bytesPerRow,
-        space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: bitmapInfo, provider: provider, decode: nil, shouldInterpolate: true, intent: .defaultIntent) else { return nil }
+        guard let cgImage = CGImage(width: width,
+                                    height: height,
+                                    bitsPerComponent: 8,
+                                    bitsPerPixel: 24,
+                                    bytesPerRow: bytesPerRow,
+                                    space: CGColorSpaceCreateDeviceRGB(),
+                                    bitmapInfo: bitmapInfo,
+                                    provider: provider,
+                                    decode: nil,
+                                    shouldInterpolate: true,
+                                    intent: .defaultIntent) else { return nil }
 
         self.init(cgImage: cgImage)
     }
@@ -74,17 +98,17 @@ private func decodeDC(_ value: Int) -> (Float, Float, Float) {
 }
 
 private func decodeAC(_ value: Int, maximumValue: Float) -> (Float, Float, Float) {
-	let quantR = value / (19 * 19)
-	let quantG = (value / 19) % 19
-	let quantB = value % 19
+    let quantR = value / (19 * 19)
+    let quantG = (value / 19) % 19
+    let quantB = value % 19
 
-	let rgb = (
-		signPow((Float(quantR) - 9) / 9, 2) * maximumValue,
-		signPow((Float(quantG) - 9) / 9, 2) * maximumValue,
-		signPow((Float(quantB) - 9) / 9, 2) * maximumValue
-	)
+    let rgb = (
+        signPow((Float(quantR) - 9) / 9, 2) * maximumValue,
+        signPow((Float(quantG) - 9) / 9, 2) * maximumValue,
+        signPow((Float(quantB) - 9) / 9, 2) * maximumValue
+    )
 
-	return rgb
+    return rgb
 }
 
 private func signPow(_ value: Float, _ exp: Float) -> Float {
@@ -104,43 +128,43 @@ private func sRGBToLinear<Type: BinaryInteger>(_ value: Type) -> Float {
 }
 
 private let encodeCharacters: [String] = {
-    return "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~".map { String($0) }
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~".map { String($0) }
 }()
 
 private let decodeCharacters: [String: Int] = {
-	var dict: [String: Int] = [:]
-	for (index, character) in encodeCharacters.enumerated() {
-		dict[character] = index
-	}
-	return dict
+    var dict: [String: Int] = [:]
+    for (index, character) in encodeCharacters.enumerated() {
+        dict[character] = index
+    }
+    return dict
 }()
 
 extension String {
-	func decode83() -> Int {
-		var value: Int = 0
-		for character in self {
-			if let digit = decodeCharacters[String(character)] {
-				value = value * 83 + digit
-			}
-		}
-		return value
-	}
+    func decode83() -> Int {
+        var value: Int = 0
+        for character in self {
+            if let digit = decodeCharacters[String(character)] {
+                value = value * 83 + digit
+            }
+        }
+        return value
+    }
 }
 
 private extension String {
-	subscript (offset: Int) -> Character {
-		return self[index(startIndex, offsetBy: offset)]
-	}
+    subscript(offset: Int) -> Character {
+        return self[index(startIndex, offsetBy: offset)]
+    }
 
-	subscript (bounds: CountableClosedRange<Int>) -> Substring {
-		let start = index(startIndex, offsetBy: bounds.lowerBound)
-		let end = index(startIndex, offsetBy: bounds.upperBound)
-		return self[start...end]
-	}
+    subscript(bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ... end]
+    }
 
-	subscript (bounds: CountableRange<Int>) -> Substring {
-		let start = index(startIndex, offsetBy: bounds.lowerBound)
-		let end = index(startIndex, offsetBy: bounds.upperBound)
-		return self[start..<end]
-	}
+    subscript(bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ..< end]
+    }
 }
